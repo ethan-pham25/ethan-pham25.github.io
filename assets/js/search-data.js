9e92a50028c53b7a1c54266444ce730679f98e8a
ethan-pham25 <83552453+ethan-pham25@users.noreply.github.com>
2026-04-09 16:25:02 +0000

    Deploying to gh-pages from @ ethan-pham25/ethan-pham25.github.io@c3f2b1be47d8a350ba3913d84ab359be9201eb89 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,48 +9,56 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
-          title: "Projects",
-          description: "Some of the cool projects I&#39;ve worked on.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-resume",
+  },{id: "dropdown-satellite-testing",
+              title: "Satellite Testing",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/projects/1_pumpkin/";
+              },
+            },{id: "dropdown-mars-like-rover",
+              title: "Mars-like Rover",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/projects/2_rover/";
+              },
+            },{id: "dropdown-stroke-rehab-app",
+              title: "Stroke Rehab App",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/projects/3_thumbot/";
+              },
+            },{id: "dropdown-tars-replica",
+              title: "TARS Replica",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/projects/4_tars/";
+              },
+            },{id: "dropdown-l1-rocket",
+              title: "L1 Rocket",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/projects/5_rocket/";
+              },
+            },{id: "dropdown-frc-robot",
+              title: "FRC Robot",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/projects/6_frc/";
+              },
+            },{id: "nav-resume",
           title: "Resume",
           description: "My resume.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/resume/";
           },
-        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-        
-          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-          
-        },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "projects-satellite-testing",
+        },{id: "projects-satellite-testing",
           title: 'Satellite Testing',
           description: "Testing and automation projects for internship at Pumpkin Space Systems.",
           section: "Projects",handler: () => {
@@ -80,16 +88,6 @@ ninja.data = [{
           description: "FRC Team 3189 Circuit Breakers&#39; 2025 Robot",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_frc/";
-            },},{id: "teachings-data-science-fundamentals",
-          title: 'Data Science Fundamentals',
-          description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
-          section: "Teachings",handler: () => {
-              window.location.href = "/teachings/data-science-fundamentals/";
-            },},{id: "teachings-introduction-to-machine-learning",
-          title: 'Introduction to Machine Learning',
-          description: "This course provides an introduction to machine learning concepts, algorithms, and applications. Students will learn about supervised and unsupervised learning, model evaluation, and practical implementations.",
-          section: "Teachings",handler: () => {
-              window.location.href = "/teachings/introduction-to-machine-learning/";
             },},{
         id: 'social-cv',
         title: 'CV',
